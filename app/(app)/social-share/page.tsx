d816e9c40afaa1d2edc7aede51104e231d10d767
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-09-20 16:31:50 +0530

    feat: implement authentication middleware and create initial page components

---
+++ app/(app)/social-share/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SocialShare() {
+    return <div>social share</div>;
+}
+
+export default SocialShare;
--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/signin", "/signup", "/", "/home"]);
-
+const isPublicRoute = createRouteMatcher([
+    "/sign-in",
+    "/sign-up",
+    "/",
+    "/home",
+]);
 const isPublicApiRoute = createRouteMatcher(["/api/videos"]);
 
-export default clerkMiddleware((auth, req) => {
-    const { userId } = auth();
+export default clerkMiddleware(async (auth, req) => {
+    const { userId } = await auth();
     const currentUrl = new URL(req.url);
     const isAccessingDashboard = currentUrl.pathname === "/home";
     const isApiRequest = currentUrl.pathname.startsWith("/api");
